fd60951339f8e4569e15ddc475cedfa2f8392223
Adi Jain <jainadi313@gmail.com>
2025-03-31 15:41:12 +0530

    update

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require('express');
+const path = require('path');
+const { fetchData } = require('./dataFetcher');
+const indexRouter = require('./routes');
+
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Set view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/', indexRouter);
+
+module.exports = app;
